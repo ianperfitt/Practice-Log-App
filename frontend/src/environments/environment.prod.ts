918c2ba6cf8ee67dea56d4ccaf094cb967f31897
Ian Perfitt <iancp@protonnmail.com>
2024-03-17 18:30:32 -0500

    removed final slash from authURL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiURL: 'https://practice-log-6778a1cc6b23.herokuapp.com/api/v1',
-  authURL: "https://practice-log-6778a1cc6b23.herokuapp.com/"
+  authURL: "https://practice-log-6778a1cc6b23.herokuapp.com"
 };
