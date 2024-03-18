049988b9d73046cc4dc5a146212b1b4094578b44
Ian Perfitt <iancp@protonnmail.com>
2024-03-18 12:51:26 -0500

    created on handler for removeError action

--- frontend/src/app/auth/state/auth.reducers.ts
+++ frontend/src/app/auth/state/auth.reducers.ts
@@ -1,5 +1,5 @@
 import { createReducer, on } from '@ngrx/store';
-import { setError, setToken } from './auth.actions';
+import { removeError, setError, setToken } from './auth.actions';
 
 export interface AuthState {
     token: string;
@@ -15,5 +15,6 @@ export const authReducer = createReducer(
   initialState,
   on(setToken, (state, { token }) => { return {...state, token}}),
   on(setError, (state, { error }) => { return {...state, error}}),
+  on(removeError, (state, { error }) => { return {...state, error}})
 
   );
